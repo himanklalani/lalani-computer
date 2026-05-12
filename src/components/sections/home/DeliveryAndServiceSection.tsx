708e00327f057a354944aca0ec3600005fd6a909
himanklalani <himanklalani@gmail.com>
2026-05-12 11:13:29 +0530

    5.2.9.5 performance

--- src/components/sections/home/DeliveryAndServiceSection.tsx
+++ src/components/sections/home/DeliveryAndServiceSection.tsx
@@ -40,19 +40,18 @@ export function DeliveryAndServiceSection() {
           </div>
         </div>
         
-        <div className="flex-1 w-full h-[450px] sm:h-[400px] lg:h-[500px]">
+        <div className="flex-1 w-full lg:w-1/2">
           <FadeIn delay={0.4} direction="right" className="h-full">
-            <div className="w-full h-full rounded-[2rem] overflow-hidden relative shadow-[0_8px_30px_rgba(25,37,170,0.08)] bg-warm-bg2">
+            <div className="w-full h-64 sm:h-80 lg:h-full min-h-[320px] rounded-2xl overflow-hidden shadow-lg border border-primary/10 relative">
               <iframe 
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d235.872052!2d72.833117!3d18.9334801!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7d1dc9af3db65%3A0x2909c144ebb53b75!2zNTksIEphbm1hYmhvb21pIE1hcmcsIEthbGEgR2hvZGEsIEZvcnQsIE11bWJhaSwgTWFoYXJhc2h0cmEgNDAwMDAx!5e0!3m2!1sen!2sin!4v1715077067000" 
                 width="100%" 
                 height="100%" 
-                style={{ border: 0 }} 
+                style={{ border: 0, minHeight: '320px' }} 
                 allowFullScreen 
                 loading="lazy" 
                 referrerPolicy="no-referrer-when-downgrade"
                 title="Lalani Computers Office Location"
-                className="w-full h-full"
               ></iframe>
               
               <a 
